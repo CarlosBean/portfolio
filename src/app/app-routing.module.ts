ca398247b794abe9ee9721b7428c3f952aff7671
Carlos Benavides <1992cab@gmail.com>
2019-05-16 23:01:51 -0500

    chore: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './sections/about/about.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { EducationComponent } from './sections/education/education.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
