d61ff8f8198ff95aa9cdfefc0ce9e890f85f72d9
Carlos Benavides <1992cab@gmail.com>
2019-05-16 23:03:05 -0500

    chore: add about view

--- src/app/sections/about/about.component.ts
+++ src/app/sections/about/about.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  sharedElements: any[] = [
+    {
+      url: 'https://github.com/',
+      path: 'carlosbean',
+      icon: 'fab fa-github'
+    },
+    {
+      url: 'https://www.linkedin.com/in/',
+      path: 'carlos-benavides-de-la-cruz-21180b149',
+
+      icon: 'fab fa-linkedin'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
